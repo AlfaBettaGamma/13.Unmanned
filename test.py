da22b55db4cc8970f73b371ff6be71643e6e8bef
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-06-19 22:29:35 +0300

    update

---
+++ test.py
@@ -0,0 +1,20 @@
+def wait_to_time(time,red_time,green_time):
+  cycle = red_time + green_time
+  if (time % cycle) < red_time :
+    return red_time -  (time % cycle)
+  else:
+    return 0
+
+def Unmanned(L, N, track):
+  result = 0
+  for i in range(L):
+    svet = 0
+    for j in range(N):
+      if track[j][0] == i:
+        result = result + wait_to_time(result,track[j][1],track[j][2])
+        svet = 1
+    result = result + 1
+  return result
+
+
+print(Unmanned(10, 2, [ [3,5,5], [5,2,2] ] ))
